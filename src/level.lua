5ac881cc09199953b7bccb4a3f41359beeef3f94
prxdigyy <onlymite@outlook.com>
2026-01-12 08:47:27 -0500

    Adjust groundY value for level configuration

--- src/level.lua
+++ src/level.lua
@@ -9,7 +9,7 @@ Level.data = {
         name = "Rocky Planet",
         bg = "assets/backgrounds/level1.png",
         gravity = 1000,
-        groundY = 464,
+        groundY = 445,
         crystalsRequired = 5,
         crystalPositions = { {200, 380}, {300, 360}, {420, 380}, {540, 360}, {660, 380} }
     },
@@ -18,7 +18,7 @@ Level.data = {
         name = "Ice Planet",
         bg = "assets/backgrounds/level2.png",
         gravity = 800,  -- floaty
-        groundY = 464,
+        groundY = 445,
         crystalsRequired = 5,
         crystalPositions = { {160, 340}, {260, 300}, {360, 320}, {500, 280}, {700, 320}, {780, 300} }
     },
